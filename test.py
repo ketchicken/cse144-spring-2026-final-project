cd20056607b174268101fe4ff130e2f90debf682
ketchicken <rachelluo816@gmail.com>
2026-06-04 21:22:40 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import csv
 import torch
 
-def test_model(loader, model, device, outfile='submission.csv'):
+def test_model(loader, model1, model2, device, outfile='submission.csv'):
     with open(outfile, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(['ID', 'Label']) # HEADER
@@ -10,7 +10,9 @@ def test_model(loader, model, device, outfile='submission.csv'):
             for input, id in loader:
                 input = input.to(device)
                 id = id[0]
-                output = model(input)
-                writer.writerow([id, torch.argmax(output, dim=1).item()])
+                output1 = model1(input)
+                output2 = model2(input)
+                avg_prediction = 0.5 * torch.argmax(output1+output2, dim=1).item()
+                writer.writerow([id, avg_prediction])
 
 print("Completed")
